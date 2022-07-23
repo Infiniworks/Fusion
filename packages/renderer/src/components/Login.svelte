987822d7acd8cb2bab893e501e254139e5c02c11
Aarush <pandeaarush@gmail.com>
2022-07-23 15:33:47 -0400

    discovery of the underscore / deps
    and some dependencies removals

--- packages/renderer/src/components/Login.svelte
+++ packages/renderer/src/components/Login.svelte
@@ -1,94 +1,45 @@
 <script>
-import * as axios from 'axios';
-let credentials, name1, name2, auth1, auth2, selectedUser = 0, avatar;
+import * as nconf from "nconf"; 
 
-const refillData = async () => {
-    let UUID;
-    credentials = localStorage.getItem('credentials');
-    auth1 = localStorage.getItem('auth1');
-    auth2 = localStorage.getItem('auth2');
-    if (credentials != null) {
-        if (credentials === auth1) {
-            selectedUser = 1;
-            UUID = JSON.parse(auth1).profile.id
-        } else if (credentials === auth2) {
-            selectedUser = 2;
-            UUID = JSON.parse(auth2).profile.id
-        } else {
-            selectedUser = 0;
-            UUID = undefined;
-        }
-    } else {
-        selectedUser = 0;
-        UUID = undefined;
-    }
-    if (auth1 != null) {
-        console.log(auth1)
-        name1 = JSON.parse(auth1).profile.name;
-    }
-    if (auth2 != null) {
-        name2 = JSON.parse(auth2).profile.name;
-    }
-    if (UUID) {
-        axios.get(`https://playerdb.co/api/player/minecraft/uuid/${UUID}`)
-        .then((out) => {response = out.data.player;});
-        avatar = response.avatar;
-    } else {
-        avatar = "https://crafatar.com/avatars/d479b9f8-f8f8-4f8e-b8f8-f8f8f8f8f8f8";
-    }
-};
-
-const login = async (userID) => {
-    let logDat
+async function setUsername(id, loginName) {
+    let data;
     await window.api.login()
     .then((loginData) => {
-        logDat = loginData;
-        if (loginData == auth1) {
-            localStorage.setItem('credentials', auth1);
-            selectedUser = 1;
-        } else if (loginData == auth2) {
-            localStorage.setItem('credentials', auth2);
-            selectedUser = 2;
-        } else {
-            localStorage.setItem('credentials', loginData);
-            localStorage.setItem(`auth${userID}`, loginData);
-        }
-    });
-    refillData();
+        console.log(loginData);
+        data = loginData
+    })
+    loginName = data.profile.name;
+
+    let users = JSON.parse(localStorage.getItem("users"));
+
+    _.extend(users, JSON.parse(`{${loginName}: ${data}}`));
+    console.log(users)
+
+
+    // users.forEach(function(value, index, array) {
+    //     if (array[index].profile.name === loginName) {
+    //         array[index] = data;
+    //         return;
+    //     }
+    // });
 }
 
-const logout = async (userID) => {
-    localStorage.setItem(`auth${userID}`, undefined)
-    refillData();
+let users = JSON.parse(localStorage.getItem("users"));
+
+const login = async () => {
+    
 }
 
-refillData()
 </script>
 
 <main>
-    <div class="loginBox clearfix">
-        <img class="avatar" src={avatar} alt="Default Skin URL"/>
-        
-        <button class="launch" on:click={ async () => { await login(1) }}>
-            {name1}
-            <button on:click={ async () => { await logout(1) }}>
-            [X]
-            </button>
-        </button> <br>
-    
-        <button class="launch" on:click={ async () => { await login(2) }}>
-            {name2}
-            <button on:click={ async () => { await logout(2) }}>
-            [X]
-            </button>
-        </button> <br>
-    </div>
+    <button on:click={
+        async () => {
+            await login()
+        }
+    }>Login</button>
 </main>
 
 <style>
-img.avatar {
-    display: block; 
-    height: 100px; 
-    float: left;
-}
+
 </style>
