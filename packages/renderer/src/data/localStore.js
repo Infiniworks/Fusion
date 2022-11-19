5881a2f599b7bf104bb1798c7530a6122690a677
AarushX <pandeaarush@gmail.com>
2022-11-19 11:27:49 -0500

    BREAKING MACHINE SPECIFIC CHANGES

--- packages/renderer/src/data/localStore.js
+++ packages/renderer/src/data/localStore.js
@@ -2,15 +2,14 @@
 // ^ Needed since localStorage is available, but not properly found.
 // src/stores/content.js
 import { writable } from "svelte/store";
-// import { database } from "./dataTools.js";
+import { database } from "./dataTools.js";
 
-// let settings = new database("C:\\Files\\Coding\\local.json");
-// const initialSettingsState = await settings.get();
-// const fullData = initialSettingsState;
+let settings = new database("C:\\Files\\Coding\\local.json");
 
-export const data = writable(JSON.parse(localStorage.data));
+export const data = writable(await settings.get());
 
 data.subscribe((value) => {
     console.log(value);
-    localStorage.data = JSON.stringify(value);
+    settings.set(JSON.stringify(value));
+    window.dbTools.overwriteJSON("C:\\Files\\Coding\\local.json", value);
 });
